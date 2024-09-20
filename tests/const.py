b1b470c0f5ab8dcee490449a2f8c2884cfc2ee4b
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-20 15:50:23 -0700

    Tyring agian with test fixe

--- tests/const.py
+++ tests/const.py
@@ -1,5 +1,7 @@
 """Constants for Denon Video Select tests."""
 
+from homeassistant.const import CONF_NAME
+
 from custom_components.denon_video_select.const import CONF_MAIN_RECEIVER
 
-MOCK_CONFIG = {CONF_MAIN_RECEIVER: "test_main_receiver"}
+MOCK_CONFIG = {CONF_NAME: "test_name", CONF_MAIN_RECEIVER: "test_main_receiver"}
