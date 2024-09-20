904470532ff336c09f02d0ebc3b6eb5628b13a9f
Jordan Zucker <jordan.zucker@gmail.com>
2024-09-20 15:05:27 -0700

    Starting project I guess

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for Denon Video Select integration."""
---
+++ tests/const.py
@@ -0,0 +1,9 @@
+"""Constants for Denon Video Select tests."""
+from custom_components.denon_video_select.const import (
+    CONF_PASSWORD,
+)
+from custom_components.denon_video_select.const import (
+    CONF_USERNAME,
+)
+
+MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
